3ff52fa43d5a923691090fcbf116a8335c69aa12
Antônio Cruz <antoniocruzgomes880@gmail.com>
2023-11-10 17:12:26 -0300

    teste heroku

--- backchat/server.js
+++ backchat/server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "http://localhost:5173" } });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
